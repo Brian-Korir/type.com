dcd1da9a6180088d8ec8fcba25e7f57b50fe8b29
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-06-08 14:52:02 +0300

    Update typingtest.py

--- typingtest.py
+++ typingtest.py
@@ -1,1 +1,9 @@
+import time 
 
+def calculate_wpm(prompt_text, time_taken):
+  words_typed = len(prompt_text.split())
+  minutes_taken = time_taken/60
+  wpm = words_typed / minutes_taken
+  return wpm
+
+prompt = "Your typing skills grows daily here"
