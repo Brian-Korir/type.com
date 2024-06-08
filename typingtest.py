5726b3fa239f042fc74cb98da46f12bd1d9363de
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-06-08 14:58:18 +0300

    Update typingtest.py

--- typingtest.py
+++ typingtest.py
@@ -10,3 +10,9 @@ prompt = "Your typing skills grows daily here"
 start_time = time.time()
 user_input = input("type the prompt: '" + prompt +"'\n")
 end_time =time.time()
+
+
+elapsed_time= end_time - start_time
+typing_speed = calculate_wpm(user_input, elapsed_time)
+
+print("Your typing speed is approximately", round(typing_speed, 2), "words per minute")
